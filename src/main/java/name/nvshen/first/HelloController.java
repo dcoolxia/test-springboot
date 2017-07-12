7187f25d2993a66094cb64175596981f5fdcb77b
wangliang <wangliang@reflower.com.cn>
2017-07-12 17:06:12 +0800

    first

---
+++ src/main/java/name/nvshen/first/HelloController.java
@@ -0,0 +1,27 @@
+package name.nvshen.first;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+
+    protected static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping("/")
+    public String helloworld() {
+        logger.debug("访问hello");
+        return "Hello world!";
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String helloName(@PathVariable String name) {
+        logger.debug("访问helloName,Name={}", name);
+        return "Hello " + name;
+    }
+}
---
+++ src/test/java/name/nvshen/TestSpringBoot.java
@@ -0,0 +1,20 @@
+package name.nvshen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import name.nvshen.first.Application;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class) //指定spring-boot的启动类
+//@SpringApplicationConfiguration(classes = Application.class)//1.4.0之前版本使用
+public class TestSpringBoot {
+
+    @Test
+    public void test() {
+        System.out.println("run===============================================================");
+    }
+}
