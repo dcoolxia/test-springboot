eb63f2bf9960ba06ffb4e9cead2ac2603bb2126d
dcoolxia <dcoolxia@163.com>
2017-12-18 00:29:43 +0800

    增加对jsp的支持

--- src/main/java/name/nvshen/first/HelloController.java
+++ src/main/java/name/nvshen/first/HelloController.java
@@ -1,14 +1,18 @@
 package name.nvshen.first;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class HelloController {
 
@@ -18,19 +22,23 @@ public class HelloController {
     private String url;
     
     @RequestMapping("/")
-    public String helloworld() {
+    public String helloworld(ModelMap model) {
         logger.debug("访问hello");
-        return "Hello world!";
+        model.put("name", "David");
+        model.put("date", new Date());
+        return "/jsp/hello";
     }
 
     @RequestMapping("/hello/{name}")
+    @ResponseBody
     public String helloName(@PathVariable String name) {
         logger.debug("访问helloName,Name={}", name);
         return "Hello " + name;
     }
     
     @RequestMapping("/url")
+    @ResponseBody
     public String readParam() {
-        return "http:// " + url;
+        return "http://" + url;
     }
 }
